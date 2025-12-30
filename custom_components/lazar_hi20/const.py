6b7926e1f7e00e7a143d604aae17c32ee27118b1
Wilczek85 <114364309+Wilczek85@users.noreply.github.com>
2025-12-30 12:59:15 +0100

    Add constants for lazar_hi20 integration

---
+++ custom_components/lazar_hi20/const.py
@@ -0,0 +1,10 @@
+DOMAIN = "lazar_hi20"
+
+BASE_URL = "https://hkslazar.net"
+LOGIN_ENDPOINT = "/sollogin"
+DATA_ENDPOINT = "/oemSerwis"
+
+CONF_LOGIN = "login"
+CONF_PASSWORD = "password"
+
+SCAN_INTERVAL = 30
