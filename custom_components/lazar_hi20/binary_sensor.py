d133730d478eb9a256ec85362f73fca14422dd26
wilczek85 <wilczek85@gmail.com>
2025-12-31 10:18:26 +0100

    porawa binnary sensor

--- custom_components/lazar_hi20/binary_sensor.py
+++ custom_components/lazar_hi20/binary_sensor.py
@@ -1,26 +1,77 @@
 from homeassistant.components.binary_sensor import BinarySensorEntity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from .const import DOMAIN
 
-BINARY_SENSORS = {
+HEATERS = {
     "leak": "Grzałka antyzamarzaniowa",
-    "flowlvl1": "Grzałka poziom 1",
-    "flowlvl2": "Grzałka poziom 2",
-    "carter": "Grzałka karteru"
+    "flowlvl1": "Grzałka CO poziom 1",
+    "flowlvl2": "Grzałka CO poziom 2",
+    "carter": "Grzałka karteru sprężarki",
 }
+
+PUMPS = {
+    "pumpcir1": "Pompa obiegu CO 1",
+    "pumpcir2": "Pompa obiegu CO 2",
+}
+
+OTHER_BINARY = {
+    "valve4way": "Zawór 4-drogowy",
+}
+
 
 async def async_setup_entry(hass, entry, async_add_entities):
     coordinator = hass.data[DOMAIN][entry.entry_id]
-    async_add_entities(
-        LazarBinary(coordinator, k, v)
-        for k, v in BINARY_SENSORS.items()
-    )
 
-class LazarBinary(BinarySensorEntity):
-    def __init__(self, coordinator, key, name):
-        self.coordinator = coordinator
-        self.key = key
+    entities = []
+
+    for key, name in HEATERS.items():
+        entities.append(
+            LazarBinarySensor(
+                coordinator,
+                f"stat.heaters.{key}",
+                name,
+            )
+        )
+
+    for key, name in PUMPS.items():
+        entities.append(
+            LazarBinarySensor(
+                coordinator,
+                f"stat.pumps.{key}",
+                name,
+            )
+        )
+
+    for key, name in OTHER_BINARY.items():
+        entities.append(
+            LazarBinarySensor(
+                coordinator,
+                f"stat.other.{key}",
+                name,
+            )
+        )
+
+    async_add_entities(entities)
+
+
+class LazarBinarySensor(CoordinatorEntity, BinarySensorEntity):
+    def __init__(self, coordinator, path, name):
+        super().__init__(coordinator)
+        self._path = path
         self._attr_name = name
+        self._attr_unique_id = f"{coordinator.data['boxid']}_{path}"
 
     @property
     def is_on(self):
-        return bool(self.coordinator.data["stat"]["heaters"].get(self.key))
+        data = self.coordinator.data
+        if not data:
+            return None
+
+        try:
+            value = data
+            for key in self._path.split("."):
+                value = value[key]
+        except Exception:
+            return None
+
+        return bool(value)
